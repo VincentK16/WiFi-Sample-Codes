4718359729907e986566b16c75ddbb2e2144764a
Vincent Kok <vick.kok@live.com>
2021-11-09 12:08:47 +0800

    Update uKitExplore_ReceiveMessage.ino

--- 1. TTGO-uKitExplore_Basic Serial Communication/uKitExplore_ReceiveMessage/uKitExplore_ReceiveMessage.ino
+++ 1. TTGO-uKitExplore_Basic Serial Communication/uKitExplore_ReceiveMessage/uKitExplore_ReceiveMessage.ino
@@ -5,12 +5,12 @@ void setup() {
 void loop() {
   if (Serial.available() > 0)
   {
-    comdata = Serial.read();
+    comdata = Serial.read(); //this will read the data as int by default
     Serial.println(comdata);
-    if(comdata ==72){
+    if(comdata ==72){ //if (comdata='H')
         Serial.println("Hello Send");
       }
-    if(comdata ==78){
+    if(comdata ==78){ //if (comdata='N')
         Serial.println("No Send");
      }
    }
